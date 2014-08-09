28878b5470334e52ecedde8a36cb59f1b0c8d335
Erfan Noury <erfannoury@users.noreply.github.com>
2014-08-10 03:37:18 +0430

    ImageEntry class updated
    ImageAnnotations are now saved in csv format.
    Also HasAnnotation calculation method has changed.

--- Pitch-Annotator.Net/ImageEntry.cs
+++ Pitch-Annotator.Net/ImageEntry.cs
@@ -7,7 +7,6 @@ using System.IO;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
-using Newtonsoft.Json;
 
 namespace PitchAnnotator
 {
@@ -15,7 +14,7 @@ namespace PitchAnnotator
     /// This class contains information about each image entry found in the images folder and their corresponding annotation files found in the annotations folder
     /// This class also can return proper view object for to be shown in the ListViewer
     /// </summary>
-    class ImageEntry: IComparable
+    class ImageEntry : IComparable
     {
         /// <summary>
         /// Absolute path to the image file
@@ -30,7 +29,13 @@ namespace PitchAnnotator
         /// <summary>
         /// A boolean showing whether image file has annotation file already or not
         /// </summary>
-        public bool HasAnnotation;
+        public bool HasAnnotation 
+        { 
+            get
+            {
+                return File.Exists(this.AnnotationAddress) && (new FileInfo(this.AnnotationAddress).Length > 0);
+            }
+        }
 
         /// <summary>
         /// Absolute path to the annotation file
@@ -44,13 +49,12 @@ namespace PitchAnnotator
         {
             this.ImageAddress = address;
             this.ImageName = Path.GetFileNameWithoutExtension(address);
-            this.AnnotationAddress = Path.Combine(annotationPath, ImageName + ".json");
-            this.HasAnnotation = File.Exists(this.AnnotationAddress);
+            this.AnnotationAddress = Path.Combine(annotationPath, ImageName + ".csv");
         }
 
         public Grid GetListViewItem()
         {
-            Grid g = new Grid();
+            Grid g = new Grid() { Width = 290 };
             g.Background = HasAnnotation ? Brushes.LightGreen : Brushes.PaleVioletRed;
             g.Children.Add(new Label() { Content = ImageName });
             return g;
