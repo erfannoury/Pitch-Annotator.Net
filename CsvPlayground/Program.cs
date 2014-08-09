f1c8cc5d808c13f0d04f8b086f391f04f032fafe
Erfan Noury <erfannoury@users.noreply.github.com>
2014-08-10 03:42:56 +0430

    Playground projects added.

---
+++ CsvPlayground/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+namespace CsvPlayground
+{
+    class Program
+    {
+
+        /// <summary>
+        /// This class is used for mapping Line objects to proper csv output and also for converting back csv records to Line objects
+        /// </summary>
+        public sealed class LineMapping : CsvClassMap<Line>
+        {
+            public LineMapping()
+            {
+                Map(m => Line.X1Property).Index(0).Name("X1");
+                Map(m => Line.X2Property).Index(1).Name("X2");
+                Map(m => Line.Y1Property).Index(2).Name("Y1");
+                Map(m => Line.Y2Property).Index(3).Name("Y2");
+            }
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Line l1 = new Line() { X1 = 23.4, X2 = 243.23, Y1 = 23.4, Y2 = 2.00 };
+            //StreamReader textreader = File.OpenText(@"E:\Code Vault\Github\Pitch-Annotator.Net\dataset\groundTruth\test\1.csv");
+            //TextReader textreader = new StreamReader(@"E:\Code Vault\Github\Pitch-Annotator.Net\dataset\groundTruth\test\1.csv");
+            
+            
+            //CsvReader csvr = new CsvReader(textreader);
+            //var lines = new List<Line>();
+            //while(csvr.Read())
+            //{
+                //lines.Add(new Line() { X1 = csvr.GetField<float>(0), X2 = csvr.GetField<float>(1), Y1 = csvr.GetField<float>(2), Y2 = csvr.GetField<float>(3) });
+                //lines.Add(csvr.GetRecord<Line>());
+            //}
+            using (var textwriter = new StreamWriter(@"E:\Code Vault\Github\Pitch-Annotator.Net\dataset\groundTruth\test\out.csv"))
+            {
+                CsvWriter csvw = new CsvWriter(textwriter, new CsvConfiguration() { HasHeaderRecord = false });
+                csvw.WriteField<double>(l1.X1);
+                csvw.WriteField<double>(l1.X2);
+                csvw.WriteField<double>(l1.Y1);
+                csvw.WriteField<double>(l1.Y2);
+                csvw.NextRecord();
+            }
+        }
+    }
+}
--- JsonPlayground/Program.cs
+++ JsonPlayground/Program.cs
@@ -6,13 +6,95 @@ using System.Threading.Tasks;
 using Newtonsoft.Json.Converters;
 using System.Windows.Controls;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace JsonPlayground
 {
+    class myLine
+    {
+        public float X1;
+        public float X2;
+        public float Y1;
+        public float Y2;
+        public myLine(float x1, float x2, float y1, float y2)
+        {
+            this.X1 = x1;
+            this.X2 = x2;
+            this.Y1 = y1;
+            this.Y2 = y2;
+        }
+    }
+
+    public class myLineJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            //List<Line> ll = new List<Line>
+            //{
+            //                    new Line() { X1 = 12.5, Y1 = 13.4, X2 = 2.5, Y2 = 5.6 },
+            //                    new Line() { X1 = 12.25, Y1 = 183.4, X2 = 24.5, Y2 = 54.6 }
+            //};
+            //Console.WriteLine(JsonConvert.SerializeObject(ll, Formatting.Indented));
+
+
+            List<myLine> fl = new List<myLine>
+            {
+                new myLine(23,24,45,21),
+                new myLine(23,44,745,23),
+                new myLine(12,345,56,54)
+            };
+
+            Console.WriteLine(JsonConvert.SerializeObject(fl, Formatting.Indented));
+            Console.WriteLine(JsonConvert.SerializeObject(new myLine(23, 434, 12, 54), Formatting.Indented));
+
+            List<myLine> flo = JsonConvert.DeserializeObject<List<myLine>>(@"[{'X1': 383, 'X2': 574, 'Y1': 176, 'Y2': 236},
+{
+    'X1': 23.0,
+    'X2': 24.0,
+    'Y1': 45.0,
+    'Y2': 21.0
+  }]");
+            Console.WriteLine(flo);
+
+            JsonSchema schema = new JsonSchema();
+            schema.Type = JsonSchemaType.Array;
+            schema.Properties = new Dictionary<string, JsonSchema>
+            {
+                {"X1", new JsonSchema() {Type = JsonSchemaType.Float}},
+                {"Y1", new JsonSchema() {Type = JsonSchemaType.Float}},
+                {"X2", new JsonSchema() {Type = JsonSchemaType.Float}},
+                {"Y2", new JsonSchema() {Type = JsonSchemaType.Float}}
+            };
+            Console.WriteLine(schema.ToString());
+
+            JObject lobj;
+            using (StreamReader file = File.OpenText(@"E:\Code Vault\Github\Pitch-Annotator.Net\dataset\groundTruth\test\1.json"))
+            {
+                lobj = JObject.Parse(file.ReadToEnd());
+                bool valid = lobj.IsValid(schema);
+                Console.WriteLine(valid);
+            }
         }
     }
 }
