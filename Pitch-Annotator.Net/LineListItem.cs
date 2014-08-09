f70f46bbdbcb77575144ed3199adf6f14534177a
Erfan Noury <erfannoury@users.noreply.github.com>
2014-08-10 03:41:54 +0430

    LineListItem class created.
    This class is inherited from the Label class to inherit it's visual
    features.
    The only difference is that it contains a reference to an object of type
    Line.
    Using this reference, deleting a layer is implemented.

---
+++ Pitch-Annotator.Net/LineListItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Diagnostics;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+using System.IO;
+
+namespace PitchAnnotator
+{
+    /// <summary>
+    /// Class for creating appropriate ListItems for the ListView showing line layers for each image
+    /// It has the full functionality of the Label class, but also contains a reference to a Line object
+    /// </summary>
+    class LineListItem : Label
+    {
+        public Line LineReference;
+    }
+}
