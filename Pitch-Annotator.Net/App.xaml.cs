83def0ebfd5f2dc3dd09a2196ada9fd7c3f37142
Erfan Noury <erfannoury@users.noreply.github.com>
2014-08-10 04:57:20 +0430

    Changed project type to Exe
    Now application can get commandline arguments.

--- Pitch-Annotator.Net/App.xaml.cs
+++ Pitch-Annotator.Net/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 
@@ -12,5 +13,29 @@ namespace PitchAnnotator
     /// </summary>c
     public partial class App : Application
     {
+        public App()
+        {
+            TextWriterTraceListener textWriteTraceListener = new TextWriterTraceListener(Console.Out);
+            Debug.Listeners.Add(textWriteTraceListener);
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var args = e.Args;
+            foreach(var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            if(args.Length != 2)
+            {
+                Console.WriteLine("You should enter two arguments: ");
+                Console.WriteLine("Pitch-Annotator.Net.exe path/to/images/folder path/to/annotations/folder");
+                return;
+            }
+            else
+            {
+                MainWindow wnd = new MainWindow(args[0], args[1]);
+                wnd.Show();
+            }
+        }
     }
 }
